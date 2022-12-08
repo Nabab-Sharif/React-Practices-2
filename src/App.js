aed0f949e0001e35e3ec17f369975d96ba418fe2
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-12-08 23:46:25 +0600

    state crete in class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Clock from './components/Clock';
+import Clock from './components/Clock3';
 
 
 function App() {
---
+++ src/components/Clock3.js
@@ -0,0 +1,40 @@
+
+//Example 3 here
+
+import React from 'react';
+
+class Clock extends React.Component {
+
+
+  state = { date: new Date() }
+
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+
+  }
+
+
+  render() {
+    return (
+      <h1>
+        <span> Welcome {this.state.date.toLocaleTimeString(this.props.locale)} </span>
+      </h1>
+    );
+
+  }
+}
+
+export default Clock;
